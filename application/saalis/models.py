73eb2a757f957aa97b9abcfe56c1d459d772cb11
Terho Tuukka M J <tmjterho@helsinki.fi>
2019-11-12 15:50:19 +0200

    Kirjautuminen lisätty

--- application/saalis/models.py
+++ application/saalis/models.py
@@ -1,5 +1,6 @@
 from application import db
 
+
 class Saalis(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     laji = db.Column(db.String(100), nullable=False)
@@ -7,8 +8,10 @@ class Saalis(db.Model):
     paivamaara = db.Column(db.DateTime, default=db.func.current_timestamp())
     maara = db.Column(db.Integer, nullable=False)
     koordinaatit = db.Column(db.String(100), nullable=False)
-    
-    def __init__(self,laji,paikka,maara,koordinaatit):
+    account_id = db.Column(db.Integer, db.ForeignKey(
+        'account.id'), nullable=False)
+
+    def __init__(self, laji, paikka, maara, koordinaatit):
         self.laji = laji
         self.paikka = paikka
         self.maara = maara
--- application/saalis/views.py
+++ application/saalis/views.py
@@ -1,5 +1,7 @@
 from application import app, db
 from flask import render_template, request, redirect, url_for
+from flask_login import login_required,current_user
+
 from application.saalis.models import Saalis
 from application.saalis.forms import SaalisForm
 
@@ -10,11 +12,13 @@ def saalis_index():
 
 
 @app.route("/saalis/new/")
+@login_required
 def saalis_form():
     return render_template("saalis/new.html", form=SaalisForm())
 
 
 @app.route("/saalis/", methods=["POST"])
+@login_required
 def saalis_create():
     form = SaalisForm(request.form)
 
@@ -26,6 +30,7 @@ def saalis_create():
     maara = form.maara.data
     koordinaatit = form.koordinaatit.data
     t = Saalis(laji, paikka, maara, koordinaatit)
+    t.account_id = current_user.id
     db.session().add(t)
     db.session().commit()
     return redirect(url_for("saalis_index"))
