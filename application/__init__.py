c413d3409ada99ee178dfd620ce489852739d4ee
Terho Tuukka M J <tmjterho@helsinki.fi>
2019-11-12 17:06:59 +0200

    Tuki herokun tietokannalle  lisätty

--- application/__init__.py
+++ application/__init__.py
@@ -1,34 +1,43 @@
+from flask_login import LoginManager
+from os import urandom
+from application.auth.models import User
+from application.auth import views
+from application.auth import models
+from application.saalis import views
+from application.saalis import models
+from application import views
+import os
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+
 app = Flask(__name__)
 
-from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///saalis.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///saalis.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
-from application import views
-
-from application.saalis import models
-from application.saalis import views
-
-from application.auth import models
-from application.auth import views
 
 # kirjautuminen
-from application.auth.models import User
-from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
 
-from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+
+try:
+    db.create_all()
+except:
+    pass
