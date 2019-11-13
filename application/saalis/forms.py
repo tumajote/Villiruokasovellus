cb67fcd3ce5495acc376df3c64c945879d593969
Terho Tuukka M J <tmjterho@helsinki.fi>
2019-11-13 21:59:33 +0200

    CRUD toiminnallisuus lisätty saalis taulullle

--- application/saalis/forms.py
+++ application/saalis/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, validators
+from wtforms import StringField, IntegerField,BooleanField, validators
 
 class SaalisForm(FlaskForm):
     laji = StringField("Laji", [validators.Length(min=2)])
     paikka = StringField("Paikka")
     maara = IntegerField("Määrä")
     koordinaatit = StringField("Koordinaatit")
+    poista = BooleanField("Poista")
 
  
     class Meta:
         csrf = False
+        
--- application/saalis/views.py
+++ application/saalis/views.py
@@ -1,12 +1,12 @@
 from application import app, db
 from flask import render_template, request, redirect, url_for
-from flask_login import login_required,current_user
+from flask_login import login_required, current_user
 
 from application.saalis.models import Saalis
 from application.saalis.forms import SaalisForm
 
 
-@app.route("/saalis", methods=["GET"])
+@app.route("/saalis")
 def saalis_index():
     return render_template("saalis/list.html", saaliit=Saalis.query.all())
 
@@ -17,13 +17,13 @@ def saalis_form():
     return render_template("saalis/new.html", form=SaalisForm())
 
 
-@app.route("/saalis/", methods=["POST"])
+@app.route("/saalis/new", methods=["POST"])
 @login_required
 def saalis_create():
     form = SaalisForm(request.form)
 
     if not form.validate():
-        return render_template("saalis/new.html", form = form)
+        return render_template("saalis/new.html", form=form)
 
     laji = form.laji.data
     paikka = form.paikka.data
@@ -34,3 +34,48 @@ def saalis_create():
     db.session().add(t)
     db.session().commit()
     return redirect(url_for("saalis_index"))
+
+
+@app.route("/saalis/<saalis_id>/", methods=["GET", "POST"])
+@login_required
+def saalis_editForm(saalis_id):
+    saalis = Saalis.query.get(saalis_id)
+
+    form = SaalisForm(request.form)
+
+    if request.method == 'POST':
+
+        if form.poista:
+            db.session().delete(saalis)
+            db.session().commit()
+            return redirect(url_for("saalis_index"))
+
+        muutos = False
+        if saalis.laji != form.laji.data:
+            saalis.laji = form.laji.data
+            muutos = True
+
+        if saalis.paikka != form.paikka.data:
+            saalis.paikka = form.paikka.data
+            muutos = True
+
+        if saalis.maara != form.maara.data:
+            saalis.maara = form.maara.data
+            muutos = True
+
+        if saalis.koordinaatit != form.koordinaatit.data:
+            saalis.koordinaatit = form.koordinaatit.data
+            muutos = True
+
+        if not form.validate():
+            return render_template("saalis/edit.html", saalis=saalis, form=form)
+
+        if muutos:
+            db.session().commit()
+
+    form.laji.data = saalis.laji
+    form.paikka.data = saalis.paikka
+    form.maara.data = saalis.maara
+    form.koordinaatit.data = saalis.koordinaatit
+
+    return render_template("saalis/edit.html", saalis=saalis, form=form)
