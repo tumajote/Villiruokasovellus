68c16c9e9f2e34bfccaf3e89a5b877e671286fa4
Terho Tuukka M J <tuukka.mj.terho@helsinki.fi>
2019-11-26 09:49:26 +0200

    Rekisteröinti

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField,validators
   
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
   
+    class Meta:
+        csrf = False
+
+
+class SignInForm(FlaskForm):
+    username = StringField("Username", [validators.Length(min=3, max=10)])
+    password = PasswordField("Password", [validators.Length(min=6)])
+    confirmPassword = PasswordField("Confirm password")
+
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
+from application.auth.forms import LoginForm, SignInForm
 from application.auth.models import User
-from application.auth.forms import LoginForm
 
 
 @app.route("/auth/login", methods=["GET", "POST"])
@@ -18,7 +18,7 @@ def auth_login():
         username=form.username.data, password=form.password.data).first()
     if not user:
         return render_template("auth/loginform.html", form=form,
-                               error="No such username or password")
+                               error="Käyttäjänimi tai salasana on väärin!")
 
     login_user(user)
     return redirect(url_for("index"))
@@ -27,4 +27,30 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/auth/signin", methods=["GET", "POST"])
+def auth_signin():
+    if request.method == "GET":
+        return render_template("auth/signinform.html", form=SignInForm())
+
+    form = SignInForm(request.form)
+    # mahdolliset validoinnit
+
+    if not form.validate():
+        return render_template("auth/signinform.html", form=form())
+
+    if not form.password.data == form.confirmPassword.data:
+        return render_template("auth/signinform.html", form=form,
+                               error="Salasana ja salasananvarmistus eivät täsmää!")
+
+    if User.query.filter_by(username=form.username.data).first():
+        return render_template("auth/signinform.html", form=form,
+                               error="Käyttäjänimi on jo käytössä!")
+
+    user = User(form.username.data, form.username.data, form.password.data)
+    db.session().add(user)
+    db.session().commit()
+    login_user(user)
     return redirect(url_for("index"))
